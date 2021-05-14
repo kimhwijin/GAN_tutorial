33559f6dff77fd142ee9d12c427c65d033f25d37
kimhwijin <azxsdc0160@naver.com>
2021-05-14 23:38:39 +0900

    implement train func

--- train.py
+++ train.py
@@ -18,14 +18,29 @@ def train(epochs=1, batchSize=128):
             imageBatch = X_train[np.random.randint(0,X_train.shape[0],size=batchSize)]
 
             generatedImages = generator.predict(noise)
-            X = np.concatenate([imageBatch,generatedImages])
 
-            #생성된 것과 실제 이미지의 레이블
-            yDis = np.zeros(2*batchSize)
-            #편파적 레이블 평활화
-            yDis[:batchSize] = 0.9
 
-            #판별기 훈련
-            discriminator.trainable = True
-            dloss = discriminator.tarin_on_batch(X,yDis)
-            
+        X = np.concatenate([imageBatch,generatedImages])
+
+        #생성된 것과 실제 이미지의 레이블
+        yDis = np.zeros(2*batchSize)
+        #편파적 레이블 평활화
+        yDis[:batchSize] = 0.9
+
+        #discriminator 훈련
+        discriminator.trainable = True
+        dloss = discriminator.tarin_on_batch(X,yDis)
+
+        #generator 훈련
+        noise = np.random.normal(0,1,size=[batchSize, randomDim])
+        yGen = np.ones(batchSize)
+        discriminator.trainable = False
+        gloss = gan.train_on_batch(noise, yGen)
+
+    dLosses.append(dloss)
+    gLosses.append(gloss)
+
+    if e == 1 or e % 20 == 0:
+        saveGeneratedImages(e)
+    
+
