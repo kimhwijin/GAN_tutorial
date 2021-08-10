69c7ec314950b631ae15f5fb42b62d4a1cfa5099
kimhwijin <azxsdc0160@naver.com>
2021-08-10 18:54:01 +0900

    ReinforcementLearning

---
+++ ReinforcementLearning/gymtest.py
@@ -0,0 +1,22 @@
+import gym
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+env_name = "Breakout-v0"
+env = gym.make(env_name)
+
+frames = []
+env.reset()
+done = False
+for _ in range(300):
+    frames.append(env.render(mode='rgb_array'))
+    obs, reward, done, _ = env.step(env.action_space.sample())
+    if done:
+        break
+
+patch = plt.imshow(frames[0])
+plt.axis('off')
+def animate(i):
+    patch.set_data(frames[i])
+    anim = animation.FuncAnimation(plt.gcf(), animate, frames=len(frames), interval=10)
+    anim.save("random_agent.gif", writer='imagemagick')
+
