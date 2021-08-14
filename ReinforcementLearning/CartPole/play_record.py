372e72b1b49844c4fe7b0bc969082167e0c81e68
kimhwijin <azxsdc0160@naver.com>
2021-08-14 21:27:30 +0900

    record 2 test

--- ReinforcementLearning/CartPole/play_record.py
+++ ReinforcementLearning/CartPole/play_record.py
@@ -6,7 +6,7 @@ import os
 
 images = []
 record_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'records')
-model_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'saved_model/my_model')
+model_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'saved_model/CartPole-v0')
 model = tf.keras.models.load_model(model_dir)
 
 
@@ -16,10 +16,13 @@ env = gym.wrappers.Monitor(env, record_dir, force=True)
 
 done = False
 observation = env.reset()
-for _ in range(1000):
-    action = env.action_space.sample()
+i = 0
+while i <= 5:
+    observation = np.expand_dims(observation, axis=0)
+    action = np.argmax(model.predict(observation))
     observation, _, done, _ = env.step(action)
     if done:
+        i += 1
         observation = env.reset()
 
 env.reset()
@@ -42,4 +45,3 @@ while not done:
 imageio.mimsave('CartPole-v0.gif', [np.array(img) for i, img in enumerate(images) if i%2 == 0], fps=29)
 env.close()
 '''
-
